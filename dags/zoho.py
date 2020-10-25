6b80dd9da4a1ea8626c0627e325cec9d4d610679
Drew <dakindre4@gmail.com>
2020-10-25 22:40:04 +0100

    restructure org creation

--- dags/zoho.py
+++ dags/zoho.py
@@ -9,28 +9,19 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 import extraction.google_sheet_extraction as google_sheet_extraction
 
-
-
-item_args = {
-    'name': 'test_2',
-    'start_date': days_ago(2)
-}
-
-dag = DAG(
-    dag_id='zoho',
-    default_args=item_args,
-    description='A dag to interact with zoho API',
-    schedule_interval=None
-)
-
-
 class ApiRequest:
-  _base_url = "https://inventory.zoho.com/api/v1/organizations?="
+  _base_url_access = 'https://accounts.zoho.com/oauth/v2/token?refresh_token='
+  _base_url = "https://inventory.zoho.com/api/v1/"
 
-  def __init__(self):
-    self.zoho_client_id, self.zoho_client_secret, self.zoho_refresh_token = self.get_creds()
-    self.zoho_access_token = self.get_access_token()
-    self.zoho_org = self.get_zoho_org()
+  def __init__(self, org_data):
+    self.client_id, self.client_secret, self.refresh_token = self.get_creds()
+    self.access_token = self.get_access_token()
+    self.header = {
+      'Authorization': f'Zoho-oauthtoken {self.access_token}',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    self.org = org_data['org_id']
+    self.org_data = org_data
 
 
   def get_creds(self):
@@ -45,33 +36,37 @@ class ApiRequest:
       return zoho_client_id, zoho_client_secret, zoho_refresh_token
 
   def get_access_token(self):
-    url = f'https://accounts.zoho.com/oauth/v2/token?refresh_token={self.zoho_refresh_token}&client_id={self.zoho_client_id}&client_secret={self.zoho_client_secret}&redirect_uri=http://www.zoho.com/books&grant_type=refresh_token'
+    url = f'{self._base_url_access}{self.refresh_token}&client_id={self.client_id}&client_secret={self.client_secret}&redirect_uri=http://www.zoho.com/books&grant_type=refresh_token'
     response = requests.request("POST", url, data = {})
 
     return response.json()['access_token']
 
 
-  def get_zoho_org(self):
-    url = "https://inventory.zoho.com/api/v1/organizations?="
-    headers = {
-      'Authorization': f'Zoho-oauthtoken {self.zoho_access_token}'
-    }
+  def set_org(self):
+    response = requests.request("GET", f'{self._base_url}organizations?=', headers=self.header)
+    org_list = [x.get('organization_id') for x in response.json()['organizations']]
 
-    response = requests.request("GET", url, headers=headers, data = {})
-    return response.json()['organizations'][0]['organization_id']
+    if str(self.org) not in org_list:
+      self.create_org()
 
-  def create_zoho_org(self):
-    url = "https://inventory.zoho.com/api/v1/organizations?="
-    headers = {
-      'Authorization': f'Zoho-oauthtoken {self.zoho_access_token}'
-    }
+  def create_org(self):
+    cons_url = f'{self._base_url}organizations?organization_id={self.org}'
+    org_name = self.org_data['org_name']
+
+    payload = {
+      'JSONString': f'{"name": "{org_name}", "currency_code": "EUR", "time_zone": "PST"}'}
+
+    print(payload)
+    # response = requests.request("POST", cons_url, headers=self.header)
+
+    # print(response.json())
 
 
   def create_item(self):
-    url = f'https://inventory.zoho.com/api/v1/items?organization_id={self.zoho_org}'
+    url = f'https://inventory.zoho.com/api/v1/items?organization_id={self.org}'
     print(url)
     headers = {
-      'Authorization': f'Zoho-oauthtoken {self.zoho_access_token}',
+      'Authorization': f'Zoho-oauthtoken {self.access_token}',
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     }
 
@@ -81,9 +76,9 @@ class ApiRequest:
     print(response.json())
 
   def update_zoho_item(self):
-    url = f'https://inventory.zoho.com/api/v1/items?organization_id={self.zoho_org}'
+    url = f'https://inventory.zoho.com/api/v1/items?organization_id={self.org}'
     headers = {
-      'Authorization': f'Zoho-oauthtoken {self.zoho_access_token}'
+      'Authorization': f'Zoho-oauthtoken {self.access_token}'
     }
 
     payload = {'JSONString': '{"name": ""}'}
@@ -91,42 +86,56 @@ class ApiRequest:
     response = requests.request("POST", url, headers=headers, data = payload)
     print(response.json())
 
-def api_call():
-    api = ApiRequest()
-    api.create_item()
 
-class DagFactory:
-  def __init__(self, dag_id, dag_data, schedule):
-        self.dag_id = dag_id
-        self.dag_data = dag_data
-        self.schedule = schedule
-        self.default_args = {
-            'owner': 'Drew',
-            'start_date': days_ago(2)
-        }
-
-  @contextmanager
-  def _dag(self):
-      with DAG(self.dag_id, default_args=self.default_args, schedule_interval=self.schedule) as dag:
-          yield dag
-
-  def org_dag(self):
-    with self._dag() as dag:
-      task = PythonOperator(
-        task_id=self.dag_data['item_name'],
-        python_callable=api_call,
-        op_kwargs={'random_base': },
-        dag=dag
-      )
-    return dag
+def main(org_data, source_data):
+    api = ApiRequest(org_data)
+    api.set_org()
+    # api.update_zoho()
 
 
+if __name__ == "__main__":
+    main(
+      org_data = '53793142878',
+      source_data = {}
+    )
+    
+# class DagFactory:
+#   def __init__(self, dag_id, org_data, source_data, schedule):
+#         self.dag_id = dag_id
+#         self.org_data = org_data
+#         self.source_data = source_data
+#         self.schedule = schedule
+#         self.default_args = {
+#             'owner': 'Drew',
+#             'start_date': days_ago(2)
+#         }
 
-organizations, source_of_truth = google_sheet_extraction.get_sheet_data()
-for orgs in organizations:
-  dag_factory = DagFactory(
-    dag_id = orgs['org_id'],
-    dag_data = source_of_truth,
-    schedule = '@daily'
-  )
-  dag_factory.org_dag()
+#   @contextmanager
+#   def _dag(self):
+#       with DAG(self.dag_id, default_args=self.default_args, schedule_interval=self.schedule) as dag:
+#           yield dag
+
+#   def org_dag(self):
+#     with self._dag() as dag:
+#       task = PythonOperator(
+#         task_id=self.dag_id,
+#         python_callable=main,
+#         op_kwargs={
+#           'org_data': self.org_data,
+#           'source_data': self.source_data
+#         },
+#         dag=dag
+#       )
+#     return dag
+
+
+
+# organizations, source_of_truth = google_sheet_extraction.get_sheet_data()
+# for orgs in organizations:
+#   dag_factory = DagFactory(
+#     dag_id = orgs['org_id'],
+#     org_data = orgs,
+#     source_data = source_of_truth,
+#     schedule = '@daily'
+#   )
+#   dag_factory.org_dag()
